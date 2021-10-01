5e22883fcf98bbe100b5f2474070c711287c1e0b
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-09-30 20:59:39 -0400

    myMap implementation

--- assignment2.js
+++ assignment2.js
@@ -1,4 +1,4 @@
-// FOR EACH //
+// FOR EACH: executes a provided function once for each array element //
 Array.prototype.myEach = function(callbackFn) {
     for (let i = 0; i < this.length; i++){
         if(this[i] === undefined) continue; // skip if undefined
@@ -6,9 +6,14 @@ Array.prototype.myEach = function(callbackFn) {
     }
 };
 
-// MAP //
-Array.prototype.myMap = function() {
-
+// MAP: creates a new array populated with the results of calling a provided function on every element in the calling array.//
+Array.prototype.myMap = function(callbackFn) {
+    const newNum = [];
+    for( let i = 0; i < this.length; i++){
+        if(this[i] === undefined) continue;
+        newNum[i] = callbackFn(this[i],i, this); // assign new values from callback func to new array
+    }
+    return newNum;
 };
 
 // FILTER //
